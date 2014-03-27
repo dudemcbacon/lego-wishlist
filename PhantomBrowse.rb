058ebde911e3c4f3bbc2e2d8d04416133984b56e
Brandon Burnett <gentoolicious@gmail.com>
2014-03-27 21:59:49 +0000

    Added PhantomBrowse.rb to maybe drive webkit with Phantom.

--- PhantomBrowse.rb
+++ PhantomBrowse.rb
@@ -1,23 +1,25 @@
 require "rubygems"
 require "selenium-webdriver"
+require "pry"
 
 module LegoWishlist
-  class SeleniumBrowse
+  class PhantomBrowse
     
     def initialize(url)
-      driver = Selenium::WebDriver.for(:remote, :url => url)
+      @driver = Selenium::WebDriver.for(:remote, :url => url)
     end
 
     def login
-      driver.navigate.to "http://google.com"
-      element = drive.find_element(:name, 'q')
+      binding.pry
+      @driver.navigate.to "http://google.com"
+      element = @driver.find_element(:name, 'q')
       element.send_keys "PhantomJS"
       element.submit
-      puts driver.title
-      driver.quit
+      puts @driver.title
+      @driver.quit
     end
   end
 end
 
-a = LegoWishlist::SeleniumBrowse.new("http://localhost:9134")
+a = LegoWishlist::PhantomBrowse.new("http://localhost:9134")
 a.login
